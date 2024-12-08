3d660df4caca499ea5b0e230a874195b0c1b2e92
Jakub Vokoun <javok@debian-kuba-old-pc.debian>
2024-12-08 16:28:21 +0100

    ukol 3,4 J

---
+++ ukol3.py
@@ -0,0 +1,58 @@
+"""
+V předchozím úkole se Ti možná nelíbilo,
+že se výsledek vždycky napsal až na další řádku.
+To se dá opravit takhle:
+"""
+
+x = 8
+y = 5
+rozdil = x - y
+
+print(f"x - y = {rozdil}")
+
+"""
+Teď jsme udělali trochu chytřejší print.
+Tím, že je tam f mu říkáme, že to co následuje chcem zformátovat.
+A co to znamená? do složených závorek {} můžu napsat nějakou proměnnou
+a ono ji to normálně napíše.
+
+Dej si jenom pozor na to, že za f musí být uvozovky, a na konci taky.
+Takže: print(f"něco")
+
+Možná se Ti taky nelíbí, že když se to vypíše, tak tam je napsáno 
+prostě "x - y" a ne jejich skutečné hodnoty "8 - 5".
+Teď už ale víš, jako to změnit! Zkusíš to?
+
+Rada - jak napsat složené závorky:
+- na české klávesnici:
+    Vpravo od mezerníku je AltGr, ten drž a u toho kliknu na B a pak N.
+- na anglické klávesnici:
+    Drž shift a klikni tam, kde je 'ú' (kousek od enteru).
+"""
+
+print(f"x - y = {rozdil}")
+
+"""
+Až si to vyzkoušíš, mám pro Tebe další super novinku!
+Do složených závorek se dá napsat víc, než jenom proměnná.
+Můžeš do nich napsat celou funkci/matematickou operaci:
+"""
+
+print(f"x krát y je {x * y}")
+
+"""
+Zkus si na dalších řádkách pomocí printu napsat
+jak dopadnou všechny čtyři operace mezi číslem 1 a 2
+bez toho, aby sis je někam ukládal.
+To, co se pak napíše by mělo vypadat asi takhle:
+77 + 17 = 94
+77 - 17 = 60
+...
+"""
+cislo1 = 77
+cislo2 = 17
+
+print(f"")
+print(f"")
+print(f"")
+print(f"")
---
+++ ukol4.py
@@ -0,0 +1,18 @@
+"""
+A je tu ULTIMÁTNÍ VÝZVA, ve které spojíš všechno, co ses zatím naučil!
+
+Výzva:
+    Napiš program, který bude mít proměnné:
+        t (čas v hodinách)
+        s (dráha v kilometrech)
+        v (rychlost v kilometrech za hodinu)
+    A podle času a dráhy bude umět dopočítat rychlost.
+    
+    Až rychlost spočítá, napíše ji nějak pěkně, třeba:
+    "Rychlost v = ..."
+
+    Použij čas = 2 hodiny a dráha = 100 km, 
+    schválně, jestli Ti vyjde to samý, co mě...
+
+Tak do toho!
+"""
