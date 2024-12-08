0b3a730d179376e05602c709fbc2f920ad4a2564
Jakub Vokoun <javok@debian-kuba-old-pc.debian>
2024-12-08 15:56:25 +0100

    ukol1 J

--- pthn.py
+++
@@ -1,4 +0,0 @@
-i:int = 456
-
-for i in range(1000, 0, -1):
-  print(i, end=",")
---
+++ ukol1.py
@@ -0,0 +1,37 @@
+# všechno co napíšeš je kód
+# pokud není na začátku řádky symbol #
+# ten udělá ze řádky komentář
+"""
+Druhá možnost jak dělat komentáře,
+je pomocí uvozovek: 
+3 na začátku a 3 na konci komentáře,
+nemusíš dělat, na každém řádku
+"""
+
+# vytvářím proměnnou 'x', a říkám, že v ní bude číslo 4
+x = 4
+
+# napíšu hodnotu proměnné 'x'
+print(x)
+# print je z angličtiny, znamená to vytisknout
+
+# to, že 'x' je proměnná znamená, že se její hodnota může měnit
+x = "ahoj"
+# abys viděl, že je to pravda, znovu napíšu hodnotu 'x':
+print(x)
+# a už je tam napsaný něco jinýho než 4, žejo?
+
+# většinou asi budeš používat proměnné, které mají stejný typ
+# třeba čísla:
+cislo1 = 42
+cislo2 = 7
+cislo3 = cislo1 / cislo2
+print(cislo3)
+# víš, co se napíše teď?
+
+# dají se používat i jiné typy proměnných, ale o tom až někdy příště
+
+"""
+Jak tenhle kód spustit?
+Klikni na F5, kdyby se to ještě na něco ptalo, tak dej vždycky jenom enter.
+"""
